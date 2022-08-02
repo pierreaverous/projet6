b886abc0a7b002b7f587f7b24c9119f6859ed47b
averous <pierreaverous42@gmail.com>
2022-08-02 14:55:49 +0200

    changement des script

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -14,13 +14,12 @@ async function getPhotographers() {
 
 
 }
-const photographers = []
-console.log(photographers)
+
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
-    photographers.map((photographer) => {
+    photographers.forEach((photographer) => {
         const photographerModel = photographerFactory(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
